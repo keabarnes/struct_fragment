2e83bd22a3dbc01e4da298527046bc2c0a2d4098
Kea Barnes <kb@Keas-MacBook-Pro.local>
2019-09-16 20:46:04 +0200

    clippy updates

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ fn struct_content(struct_name: &syn::Ident, fields: &syn::FieldsNamed, not_inser
     fields.named.iter().for_each(|field| {
         let field_name = &field.clone().ident.unwrap().to_string();
         let has_not_inserted_attr = field.attrs.iter().any(|attr| attr.parse_meta().unwrap().name() == "not_inserted");
-        let in_not_inserted_list = not_inserted_fields.clone().unwrap_or(vec![]).contains(&field_name);
+        let in_not_inserted_list = not_inserted_fields.clone().unwrap_or_else(|| vec![]).contains(&field_name);
         if !in_not_inserted_list && !has_not_inserted_attr {
             new_fields.push(field);
         }
@@ -58,7 +58,7 @@ fn impl_insertabl_struct_macro(ast: &syn::DeriveInput) -> proc_macro::TokenStrea
             },
             Meta::NameValue(MetaNameValue{ref ident, ref lit, ..}) if ident == "not_inserted_csv" => {
                 if let Lit::Str(lit) = lit {
-                    not_inserted_fields = Some(lit.value().split(",").map(|field| {
+                    not_inserted_fields = Some(lit.value().split(',').map(|field| {
                         String::from(field.trim())
                     }).collect());
                 }
@@ -76,11 +76,11 @@ fn impl_insertabl_struct_macro(ast: &syn::DeriveInput) -> proc_macro::TokenStrea
             Fields::Named(ref fields) => {
                 struct_content(&struct_name, fields, &not_inserted_fields, struct_prelude)
             },
-            _ => panic!(format!("Unit structs cannot use derive(IteratorStruct)")),
+            _ => panic!("Unit structs cannot use derive(IteratorStruct)".to_owned()),
         },
-        _ => panic!(format!(
-            "Only structs can use derive(InsertableStruct)",
-        )),
+        _ => panic!(
+            "Only structs can use derive(InsertableStruct)".to_owned()
+        ),
     };
 
     block.into()
